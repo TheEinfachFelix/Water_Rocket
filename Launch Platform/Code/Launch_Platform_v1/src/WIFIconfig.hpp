15e0ef3f88fde69524be8d4ba738a42f6a9a697f
TheEinfachFelix <the.einfach.felix@gmail.com>
2023-12-11 18:25:57 +0100

    some changes

--- Launch Platform/Code/Launch_Platform_v1/src/WIFIconfig.hpp
+++ Launch Platform/Code/Launch_Platform_v1/src/WIFIconfig.hpp
@@ -2,10 +2,10 @@
 
 #include <cinttypes>
 
-const char* ssid = "PW5B";
-const char* password = "51823423816189225268";
-//const char* ssid = "pw:1234";
-//const char* password = "14082005";
+//const char* ssid = "PW5B";
+//const char* password = "51823423816189225268";
+const char* ssid = "pw:1234";
+const char* password = "14082005";
 
 const char* dns_adresse = "rampe";
 
