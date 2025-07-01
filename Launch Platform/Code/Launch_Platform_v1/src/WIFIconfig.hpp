40abfeb0b86a2f7e2ee94c638a0db707c9bc2784
TheEinfachFelix <74210839+TheEinfachFelix@users.noreply.github.com>
2025-07-01 19:48:58 +0200

    changed wifi pw

--- Launch Platform/Code/Launch_Platform_v1/src/WIFIconfig.hpp
+++ Launch Platform/Code/Launch_Platform_v1/src/WIFIconfig.hpp
@@ -5,7 +5,7 @@
 //const char* ssid = "PW5B";
 //const char* password = "51823423816189225268";
 const char* ssid = "pw:1234";
-const char* password = "14082005";
+const char* password = "12345678";
 
 const char* dns_adresse = "rampe";
 
